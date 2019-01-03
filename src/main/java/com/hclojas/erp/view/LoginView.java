89a59fc541f1e4efb214138ef9fd1e130e535c61
Helber Cardoso Lemos <info.helber@gmail.com>
2019-01-03 02:36:57 -0200

    #refs 030120190136 layout

--- src/main/java/com/hclojas/erp/view/LoginView.java
+++ src/main/java/com/hclojas/erp/view/LoginView.java
@@ -2,16 +2,12 @@ package com.hclojas.erp.view;
 
 import java.io.Serializable;
 
-
-import org.apache.tomcat.util.security.MD5Encoder;
-
-
 public class LoginView implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	private String login;
-	private MD5Encoder senha;
+	//private MD5Encoder senha;
 	
 	
 	public void acess() {
@@ -29,13 +25,6 @@ public class LoginView implements Serializable{
 	}
 
 
-	public MD5Encoder getSenha() {
-		return senha;
-	}
-
-
-	public void setSenha(MD5Encoder senha) {
-		this.senha = senha;
-	}
+	
 
 }
