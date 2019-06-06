e12400f5ab031515669b4a526b8d868ea80e0ea6
Helber Cardoso <helber@infox.com.br>
2019-06-06 14:49:02 -0300

    Atualização Codigo

--- src/main/java/com/hclojas/erp/model/Dominio.java
+++ src/main/java/com/hclojas/erp/model/Dominio.java
@@ -2,28 +2,25 @@ package com.hclojas.erp.model;
 
 import java.io.Serializable;
 
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.SequenceGenerator;
-//import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
-//import org.hibernate.annotations.NotFound;
-
-//@Entity
-//@Table(name="tb_dominio" , schema="hclojas")
+@Entity
+@Table(name="tb_dominio" , schema="hclojas")
 public class Dominio implements Serializable{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-//	@Id
-//	@SequenceGenerator(name="GENERATOR" ,sequenceName="sq_dominio",initialValue=1, allocationSize=1 ,schema="hclojas")
-//	@GeneratedValue(generator="GENERATOR" , strategy=GenerationType.SEQUENCE)
-//	@Column(name="id_dominio")
+	@Id
+	@SequenceGenerator(name="GENERATOR" ,sequenceName="sq_dominio",initialValue=1, allocationSize=1 ,schema="hclojas")	
+	@GeneratedValue(generator="GENERATOR" , strategy=GenerationType.SEQUENCE)
+	@Column(name="id_dominio")
 	private Integer id;
 	
 //	@Column(name="nm_dominio",nullable=false,unique=false)
--- src/main/java/com/hclojas/erp/view/LoginView.java
+++ src/main/java/com/hclojas/erp/view/LoginView.java
@@ -1,20 +1,33 @@
 package com.hclojas.erp.view;
 
 import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
 
+
+
+@ManagedBean(name = "loginView")
+@ViewScoped
 public class LoginView implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	private String login;
-	//private MD5Encoder senha;
+	private String senha;
+	private boolean checkCaptcha;
 	
 	
 	public void acess() {
-		
+		FacesMessage mensagem = new FacesMessage(FacesMessage.SEVERITY_INFO,"Correct","Marcou!");
+		FacesContext.getCurrentInstance().addMessage(null,mensagem);
 	}
 
-
+    public void validarCaptcha() {
+    	
+    }
 	public String getLogin() {
 		return login;
 	}
@@ -22,6 +35,27 @@ public class LoginView implements Serializable{
 
 	public void setLogin(String login) {
 		this.login = login;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean isCheckCaptcha(ValueChangeEvent valueChagedEvent) {
+		if (valueChagedEvent.getOldValue().equals(false)) {
+			checkCaptcha=false;
+		}
+		return checkCaptcha;
+	}
+
+	public void setCheckCaptcha(boolean checkCaptcha) {
+		this.checkCaptcha = checkCaptcha;
 	}
 
 
