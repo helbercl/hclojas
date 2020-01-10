62c6650e085f26f9dbef616b32564ef61ee84614
Helber Cardoso <helber@infox.com.br>
2020-01-10 17:10:56 -0300

    Atualização de sistema

--- src/main/java/com/hclojas/erp/model/PessoaFisica.java
+++ src/main/java/com/hclojas/erp/model/PessoaFisica.java
@@ -2,5 +2,10 @@ package com.hclojas.erp.model;
 
 public class PessoaFisica  extends Pessoa{
 	private String cpf;
-
+    public String getCpf() {
+		return cpf;
+	}
+    public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 }
--- src/main/java/com/hclojas/erp/model/PessoaJuridica.java
+++ src/main/java/com/hclojas/erp/model/PessoaJuridica.java
@@ -3,5 +3,12 @@ package com.hclojas.erp.model;
 public class PessoaJuridica{
 	
 	private String cnpj;
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 
 }
